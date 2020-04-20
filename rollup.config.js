1276d8d1b38407e2aeac27e471d1e606ce33848b
Geordie Powers <geordie.powers@gmail.com>
2020-04-20 09:33:55 -0400

    build(rollup): Start rollup integration

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import { rollup } from "rollup";
+
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+import { chromeExtension, pushReloader } from "rollup-plugin-chrome-extension";
+
+export default {
+  input: "src/manifest.json",
+  output: {
+    dir: "dist",
+    format: "esm",
+  },
+  plugins: [chromeExtension(), pushReloader(), resolve(), commonjs()],
+};
